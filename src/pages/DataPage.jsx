30bc581d94e6c83c994e181a7e52deb11f1a6d8b
Wijdan <wijdanbita@gmail.com>
2026-02-24 11:47:23 +0700

    update : setup routes react

---
+++ src/pages/DataPage.jsx
@@ -0,0 +1,15 @@
+export default function DataPage() {
+  return (
+    <>
+      {/* Bagian Header di sini  */}
+      <section>
+        <h1 className='text-blue-500'>Data Page</h1>
+      </section>
+
+      {/* Bagian Data Fetch di sini  */}
+      <section>
+
+      </section>
+    </>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePages() {
+  return (
+    <>
+      {/* Bagian Header di sini  */}
+      <h1 className='text-red-500'>Home Page</h1>
+    </>
+  )
+}
